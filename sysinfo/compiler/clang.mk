f080946d1445c9e8da0b57215e2062501ad43954
midipix <writeonce@midipix.org>
2016-02-27 15:38:43 -0500

    build system: use the term 'compiler' rather than 'toolchain' where applicable.

---
+++ sysinfo/compiler/clang.mk
@@ -0,0 +1,48 @@
+ifeq ($(CROSS_COMPILE)x,x)
+	CROSS_HOST	=
+	CROSS_HOST_SPEC =
+else
+	CROSS_HOST	= $(HOST)
+	CROSS_HOST_SPEC = --target=$(HOST)
+endif
+
+
+ifeq ($(USER_CC)x,x)
+	CC	= $(NATIVE_CC) $(CROSS_HOST_SPEC)
+else
+	CC	= $(USER_CC) $(CROSS_HOST_SPEC)
+endif
+
+ifeq ($(USER_CPP)x,x)
+	CPP	= $(NATIVE_CC) $(CROSS_HOST_SPEC) -E
+else
+	CPP	= $(USER_CPP) $(CROSS_HOST_SPEC) -E
+endif
+
+ifeq ($(USER_CXX)x,x)
+	CXX	= $(NATIVE_CC)++ $(CROSS_HOST_SPEC)
+else
+	CXX	= $(USER_CXX) $(CROSS_HOST_SPEC)
+endif
+
+
+AS		= $(CROSS_COMPILE)as
+AR		= $(CROSS_COMPILE)ar
+LD		= $(CROSS_COMPILE)ld
+NM		= $(CROSS_COMPILE)nm
+OBJDUMP		= $(CROSS_COMPILE)objdump
+RANLIB		= $(CROSS_COMPILE)ranlib
+SIZE		= $(CROSS_COMPILE)size
+STRIP		= $(CROSS_COMPILE)strip
+STRINGS		= $(CROSS_COMPILE)strings
+
+
+ADDR2LINE	= $(CROSS_COMPILE)addr2line
+COV		= $(CROSS_COMPILE)gcov
+CXXFILT		= $(CROSS_COMPILE)c++filt
+ELFEDIT		= $(CROSS_COMPILE)elfedit
+OBJCOPY		= $(CROSS_COMPILE)objcopy
+READELF		= $(CROSS_COMPILE)readelf
+
+
+CFLAGS_PIC	= -fPIC
---
+++ sysinfo/compiler/gcc.mk
@@ -0,0 +1,43 @@
+ifeq ($(USER_CC)x,x)
+	ifeq ($(CROSS_COMPILE)x,x)
+		CC = $(CROSS_COMPILE)$(NATIVE_CC)
+	else
+		CC = $(CROSS_COMPILE)gcc
+	endif
+else
+	CC	= $(USER_CC)
+endif
+
+ifeq ($(USER_CPP)x,x)
+	CPP	= $(CROSS_COMPILE)cpp
+else
+	CPP	= $(USER_CPP)
+endif
+
+ifeq ($(USER_CXX)x,x)
+	CXX	= $(CROSS_COMPILE)c++
+else
+	CXX	= $(USER_CXX)
+endif
+
+
+AS		= $(CROSS_COMPILE)as
+AR		= $(CROSS_COMPILE)ar
+LD		= $(CROSS_COMPILE)ld
+NM		= $(CROSS_COMPILE)nm
+OBJDUMP		= $(CROSS_COMPILE)objdump
+RANLIB		= $(CROSS_COMPILE)ranlib
+SIZE		= $(CROSS_COMPILE)size
+STRIP		= $(CROSS_COMPILE)strip
+STRINGS		= $(CROSS_COMPILE)strings
+
+
+ADDR2LINE	= $(CROSS_COMPILE)addr2line
+COV		= $(CROSS_COMPILE)gcov
+CXXFILT		= $(CROSS_COMPILE)c++filt
+ELFEDIT		= $(CROSS_COMPILE)elfedit
+OBJCOPY		= $(CROSS_COMPILE)objcopy
+READELF		= $(CROSS_COMPILE)readelf
+
+
+CFLAGS_PIC	= -fPIC
